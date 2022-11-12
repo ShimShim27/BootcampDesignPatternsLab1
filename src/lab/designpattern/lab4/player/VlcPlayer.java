5d88213f42e088bc78f3c3f0b5191325247864df
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-12 15:04:45 +0800

    Spacing

--- src/lab/designpattern/lab4/player/VlcPlayer.java
+++ src/lab/designpattern/lab4/player/VlcPlayer.java
@@ -2,18 +2,17 @@ package lab.designpattern.lab4.player;
 
 import lab.designpattern.lab4.base.AdvancedMediaPlayer;
 
-public class VLCPlayer  implements AdvancedMediaPlayer{
+public class VLCPlayer implements AdvancedMediaPlayer {
 
 	@Override
 	public void playVLC() {
 		System.out.println("Playing VLC");
-		
+
 	}
 
 	@Override
 	public void playMP4() {
-		
-		
+
 	}
 
 }
