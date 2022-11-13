698e6fd5a969ccea0c451b73094669f1604ebec1
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-11-13 12:26:19 +0800

    Newly added to index

---
+++ src/lab/designpattern/lab4/player/VLCPlayer.java
@@ -0,0 +1,18 @@
+package lab.designpattern.lab4.player;
+
+import lab.designpattern.lab4.base.AdvancedMediaPlayer;
+
+public class VLCPlayer implements AdvancedMediaPlayer {
+
+	@Override
+	public void playVLC() {
+		System.out.println("Playing VLC");
+	}
+
+	@Override
+	public void playMP4() {
+		
+
+	}
+
+}
